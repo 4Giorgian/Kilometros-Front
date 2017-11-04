63e7d39878331b0491b3dcb56e9a9c04324c47b5
Gianmarco <giorgian.g@gmail.com>
2017-11-03 19:04:38 -0500

    Boilerplate

---
+++ Kilometros-Front/src/components/HelloWorld.vue
@@ -0,0 +1,23 @@
+<template>
+    <h1>W.IP</h1>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+
+@import "~sass/utils/index";
+
+h1 {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  color: $primary;
+}
+</style>
